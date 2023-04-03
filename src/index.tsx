47271d7f1ba230c7a6e0e75ba29718b6febdd852
th-release <cth.release@gmail.com>
2023-04-03 09:29:25 +0900

    feat: add GlobalStyle component

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
+import { GlobalStyle } from './styles/globals';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { GlobalStyle } from './styles/globals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
