c31a996428e3f7bf4b2bdcaed1b830637da83f6e
th-release <cth.release@gmail.com>
2023-04-03 09:29:47 +0900

    feat: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/index';
 
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+      <div className='app-width'>
+        <Routes>
+          <Route path={'/'} element={<Home />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
